aefd0e11fff95f5011a7ef20530fc40a58c4dfb7
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-11-22 12:50:49 +0200

    header test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,5 +4,8 @@ import "./Header.css"
 export default function Header({
     title
 }) {
-    return <h1 className="header">{title}</h1>
+    return <>
+     <h1 data-testID = 'id' className="header">{title}</h1>
+     <h1 className="header">cat</h1>
+    </>
 }
---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,45 @@
+import { render, screen } from "@testing-library/react"
+import Header from "./Header"
+
+
+it('render title',()=>{
+    let testTitle = 'test title'
+    render(<Header title= {testTitle}/>)
+    let el  = screen.getByText(testTitle)
+    expect(el).toBeInTheDocument()
+})
+it('render title with other select method' ,()=>{
+    let testTitle = 'test title'
+    render(<Header title= {testTitle}/>)
+    let el  = screen.getByRole('heading',{name:'cat'})
+    expect(el).toBeInTheDocument()
+})
+it('render title with other select by testid' ,()=>{
+    let testTitle = 'test title'
+    render(<Header title= {testTitle}/>)
+    let el  = screen.getByTestId('id')
+    expect(el).toBeInTheDocument() 
+})
+ 
+// find
+it('render title find',async ()=>{
+    let testTitle = 'test title'
+    render(<Header title= {testTitle}/>)
+    let el  = await screen.findByText(testTitle)
+    expect(el).toBeInTheDocument()
+})
+
+// query
+it('render title query', ()=>{
+    let testTitle = 'test title'
+    render(<Header title= {testTitle}/>)
+    let el  =  screen.queryByText('dog')
+    expect(el).toBeNull()
+})
+// get all
+it('render title getAll', ()=>{
+    let testTitle = 'test title'
+    render(<Header title= {testTitle}/>)
+    let el  =  screen.getAllByRole('heading')
+    expect(el.length).toBe(2)
+})
